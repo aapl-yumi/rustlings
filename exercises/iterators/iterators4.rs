bb2c047b6e2656854d488fb1ed14c9d052460449
Yumi Izumi <me@yumiizumi.com>
2023-08-31 12:12:49 -0400

    Update

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -1,9 +1,5 @@
 // iterators4.rs
-//
-// Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -15,6 +11,11 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    let ans = match num == 1 || num == 0 {
+        true => 1,
+        false => num * factorial(num - 1),
+    };
+    ans
 }
 
 #[cfg(test)]
